a86b96b765237e08899bf805c0de2f2e06fb9f94
krityan <fatalen11@gmail.com>
2017-01-24 11:04:27 +0000

    Added references

---
+++ src/main/java/kritya/scape/Reference.java
@@ -0,0 +1,11 @@
+package kritya.scape;
+
+public class Reference {
+	
+	public static final String MODID = "Scape";
+	public static final String NAME = "Scape Overhaul Mod";
+	public static final String VERSION = "0.1";
+	
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String SERVER_PROXY_CLASS = "";
+}
