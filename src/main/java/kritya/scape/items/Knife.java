5687f5cde4cc47f3948472af6090782d9137b6f3
krityan <fatalen11@gmail.com>
2017-07-27 21:02:27 +0100

    Added knife to game, TODO: add recipe to craft knife

---
+++ src/main/java/kritya/scape/items/Knife.java
@@ -0,0 +1,11 @@
+package kritya.scape.items;
+
+import net.minecraft.item.Item;
+
+public class Knife extends Item{
+
+    public Knife(){
+        setUnlocalizedName(ScapeItems.KNIFE.getUnlocalizedName());
+        setRegistryName(ScapeItems.KNIFE.getRegistryName());
+    }
+}
